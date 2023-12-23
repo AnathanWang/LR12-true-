5c8878edb3df209c509221cc9899af8aead7ad56
Anathan Wang <144335469+AnathanWang@users.noreply.github.com>
2023-12-23 13:35:20 +0300

    Add files via upload

---
+++ Л 12 Задача 3.pas
@@ -0,0 +1,22 @@
+﻿var
+  F_in,F_out: Text;
+  S,a: String;
+begin
+ Write('S: ');
+ Readln(S);
+ Assign(F_in,'C:\input.txt');
+ Reset(F_in);
+ Assign(F_out,'C:\output.txt');
+ Rewrite(F_out);
+ While not eof(F_in) do
+  begin
+   Readln(F_in,a);
+   Writeln(F_out,a);
+  end;
+  Rewrite(F_out);
+ Writeln(F_out,S);
+ Close(F_in);
+ Close(F_out);
+ Erase(F_in);
+ Rename(F_out,'C:\ШАРАГА\Основы алгоритмизации\LR1112\данные\input.txt');
+end.
